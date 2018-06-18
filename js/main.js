f38bde9d6731b6cfd4c8f6b944dbdf18d7e16022
Zong Zheng Li <contact@zongzhengli.com>
2018-06-18 01:34:36 -0400

    Remove flicker effect

--- js/main.js
+++ js/main.js
@@ -1,25 +1,3 @@
 $(function() {
-    var OVERALL_DELAY = 500;
-    var FLICKER_DELAY = 10;
-    var FLICKER_ITERATIONS = 5;
-
-    $.fn.extend({
-       qcss: function(css) {
-          return $(this).queue(function(next) {
-             $(this).css(css);
-             next();
-          });
-       }
-    });
-
-    $('.start-hidden').each(function() {
-        var $e = $(this).delay(OVERALL_DELAY * Math.random());
-        for (var i = 0; i < FLICKER_ITERATIONS; i++) {
-            $e.qcss({opacity: 1})
-                .delay(FLICKER_DELAY)
-                .qcss({opacity: 0})
-                .delay(FLICKER_DELAY);
-        }
-        $e.qcss({opacity: 1});
-    });
+    $('.start-hidden').animate({opacity: 1});
 });
